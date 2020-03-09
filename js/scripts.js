70235c698682d8049a3a4671d4c88a4df59949ff
btanguturu <46464320+btanguturu@users.noreply.github.com>
2020-03-09 13:30:22 -0700

    fixes
    -updated svg links with pngs links
    -updated randomzing code line to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Store SVGS
 var cpt = [
-    "images/Georgiou.png",
-    "images/Kirk.png",
-    "images/Picard.png",
-    "images/Janeway.png"
+    "https://raw.githubusercontent.com/btanguturu/googledoodlev2/master/images/Georgiou.png",
+    "https://raw.githubusercontent.com/btanguturu/googledoodlev2/master/images/Kirk.png",
+    "https://raw.githubusercontent.com/btanguturu/googledoodlev2/master/images/Picard.png",
+    "https://raw.githubusercontent.com/btanguturu/googledoodlev2/master/images/Janeway.png"
 ];
 
 // Random Number Generator
-let max = 5;
+let max = 4;
 let randNum = Math.floor(Math.random() * max);
 console.log(randNum);
 
-// Change Captain SVG
-$('.capt').attr("src", "(capt[randNum])");
+// Change Captain Image
+$('#capt').attr("src", cpt[randNum]);
